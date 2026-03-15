3cc79344d0dc6c62155fd82dbaec7a6e2fcd156a
oliver.meimberg <oli@meimberg.io>
2026-03-15 23:12:21 +0100

    x

--- config/environments/development.php
+++ config/environments/development.php
@@ -9,14 +9,18 @@ use Roots\WPConfig\Config;
 use function Env\env;
 
 Config::define('SAVEQUERIES', true);
-Config::define('WP_DEBUG', true);
-Config::define('WP_DEBUG_DISPLAY', true);
+Config::define('WP_DEBUG', false);
+Config::define('WP_DEBUG_DISPLAY', false);
 Config::define('WP_DEBUG_LOG', env('WP_DEBUG_LOG') ?? true);
 Config::define('WP_DISABLE_FATAL_ERROR_HANDLER', true);
-Config::define('SCRIPT_DEBUG', true);
+Config::define('SCRIPT_DEBUG', false);
 Config::define('DISALLOW_INDEXING', true);
 
-ini_set('display_errors', '1');
+// Allow direct filesystem access for plugin/theme installation
+Config::define('FS_METHOD', 'direct');
+
+ini_set('display_errors', '0');
+error_reporting(E_ALL & ~E_DEPRECATED & ~E_STRICT);
 
 // Enable plugin and theme updates and installation from the admin
 Config::define('DISALLOW_FILE_MODS', false);
